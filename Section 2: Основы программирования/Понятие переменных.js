2a12821cdad926602a81a5a5a366d563d7c0d304
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-26 23:41:13 +0300

    up

---
+++ Section 2: Основы программирования/Понятие переменных.js
@@ -0,0 +1,13 @@
+// Переменные в языке програмирования
+console.log(1000*5+99-6);
+
+const x = 1000*5+99-6;
+console.log(x);
+
+const firstName = 'Sergiy'
+const lastName = 'Halenko'
+let age = 18; 
+
+const isAbult = true; //camelCase
+
+const _$abc10 = '';
---
+++ Section 2: Основы программирования/Типы данных в JavaScript.js
@@ -0,0 +1,26 @@
+// типы данных в JS
+undefined;
+null;
+
+// Boolean
+true;
+false;
+
+// String - строка
+'Hello world'
+"hi there"
+`Hello again`
+
+// Symbol
+Symbol(01)
+
+// Number - Число, BigInt
+19
+3.14
+NaN
+
+//Object - обьектный тип 
+// обьект 
+// масив 
+// функции
+// ошибка
