ad96b2c876e257371f60c53868afb29052255ace
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-31 11:07:45 +0300

    up

---
+++ Section 4: Про функции подробно/Функции vs процедуры.js
@@ -0,0 +1,13 @@
+// Функции vs процедуры
+// Функция
+function sum(a,b){
+    return a + b;
+}
+
+const result = sum(2,4);
+console.log(result);
+
+// Процедура
+function sum2(a,b){
+    console.log(a + b);
+}
