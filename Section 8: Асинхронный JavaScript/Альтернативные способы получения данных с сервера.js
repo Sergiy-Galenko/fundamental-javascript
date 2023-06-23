6ba8c76252b04f9935ee446bcdae289dd27ff0d6
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-06-23 23:07:58 +0300

    update

--- Section 8: Асинхронный JavaScript/Альтернативные способы получения данных с сервера.js
+++ Section 8: Асинхронный JavaScript/Альтернативные способы получения данных с сервера.js
@@ -1,10 +1,14 @@
-//https://jsonplaceholder.typicode.com/
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => {
-        if (response.ok){
-            return response.json()
-        }
-        throw new Error('Failed to fetch ');
-    })
-    .then(console.log)
-    .catch(console.error)
+// AJAX
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const request = new XMLHttpRequest();
+
+request.open('GET',url)
+
+request.responseType = 'json'; // xml,text
+
+request.send()
+request.onload = function(){
+    const data = request.response;
+    console.log(data)
+}
