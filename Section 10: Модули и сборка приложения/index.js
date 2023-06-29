bbb5a6c3add453f7da82149b0b2de8dfd2738760
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-06-29 22:18:09 +0300

    Update

--- Section 10: Модули и сборка приложения/index.js
+++ Section 10: Модули и сборка приложения/index.js
@@ -1,5 +1,6 @@
-import {sum} from './first'
-import {firstName as fn} from './second'
+import {sum} from './first';
+import {firstName as fn} from './second';
+import myName from './third';
 
 
 console.log('Hello from entry point');
