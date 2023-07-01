b0b99466c2e3a530b14b32d45650431184998611
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-07-01 09:51:07 +0300

    update

---
+++ Section 12: ООП в JavaScript/Шаблон Адаптер.js
@@ -0,0 +1,59 @@
+class User {
+  constructor(login, email) {
+    this.email = email;
+    this.login = login;
+  }
+}
+
+class Editor extends User {
+  constructor(login, email) {
+    super(login, email);
+  }
+
+  createPost(title, text) {}
+}
+
+class Admin extends User {
+  static exists = false;
+  static instance = null;
+
+  constructor(login, email) {
+    if (Admin.exists) {
+      return Admin.instance;
+    }
+    super(login, email);
+    Admin.exists = true;
+    Admin.instance = this;
+  }
+}
+
+class UserCreator {
+  static userList = {
+    user: User,
+    editor: Editor,
+    admin: Admin,
+  };
+
+  static createUser(login, email, role = "user") {
+    const Fabric = UserCreator.userList[role];
+
+    const instance = new Fabric(login, email);
+    instance.role = role;
+
+    return instance;
+  }
+}
+
+
+class UserAdapter{
+
+    static userList = {
+        user: 'user',
+        editor: 'publisher',
+        admin: 'admin',
+    }
+
+    static create(login, email, role = "user"){
+       return  UserCreator.createUser(login, email, UserAdapter.userList[role]);
+    }
+}
